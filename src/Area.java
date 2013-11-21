428dc541805b0a82cebdf69effec02b7e19f024f
gu-blanco <gu_blanco@hotmail.com>
2013-11-21 17:08:45 -0200

    Inicio do projeto

---
+++ src/Area.java
@@ -0,0 +1,6 @@
+
+public abstract class Area {
+	protected double mLatitude;
+	protected double mLongitude;
+	protected double mRaio;
+}
--- src/main.java
+++ src/main.java
@@ -5,8 +5,12 @@ public class main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Jogador Novo;
+		Novo = new Jogador("Gustavo",10);
+		System.out.println(Novo.getNome() + " - " + Novo.getRecurso());
+		Novo.subRecurso(5);
+		System.out.println(Novo.getNome() + " - " + Novo.getRecurso());
+		Novo.somaRecurso(2);
+		System.out.println(Novo.getNome() + " - " + Novo.getRecurso());
 	}
-
 }
