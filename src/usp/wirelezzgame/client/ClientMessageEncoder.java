6c3004c3e19b88068654b3d0180d0e5c78cf09cb
Bruno Orlandi <brorlandi@gmail.com>
2013-11-28 00:10:56 -0200

    Encoding de mensagens cliente e servidor implementado.

--- src/usp/wirelezzgame/client/ClientMessageEncoder.java
+++ src/usp/wirelezzgame/client/ClientMessageEncoder.java
@@ -1,5 +1,77 @@
 package usp.wirelezzgame.client;
 
+import org.json.simple.JSONAware;
+import org.json.simple.JSONObject;
+
+import usp.wirelezzgame.core.area.Area;
+
+@SuppressWarnings("unchecked")
 public class ClientMessageEncoder {
 
+	public static final int DEFENDER_AREA = 0;
+	public static final int ATACAR_AREA = 1;
+	public static final int RECUPERAR_PONTOS_AREA = 2;
+
+	public static String nomeJogador(String nome){
+		
+		JSONObject data = new JSONObject();
+		data.put("nomeJogador", nome);
+
+		return toJsonMessage(1,data);
+	}
+	
+	public static String timeJogador(int id){
+		
+		JSONObject data = new JSONObject();
+		data.put("idTime", new Integer(id));
+
+		return toJsonMessage(4,data);
+	}
+	
+	public static String interagirArea(Area a, double latitude, double longitude, int acao){
+		
+		JSONObject data = new JSONObject();
+		data.put("idArea", new Integer(a.getID()));
+		data.put("latitude", new Double(latitude));
+		data.put("longitude", new Double(longitude));
+		data.put("acao", new Integer(acao));
+
+		return toJsonMessage(7,data);
+	}
+	
+	public static String responderCaptcha(int idCaptcha, String resposta){
+		
+		JSONObject data = new JSONObject();
+		data.put("idCaptcha", new Integer(idCaptcha));
+		data.put("resposta", idCaptcha);
+
+		return toJsonMessage(9,data);
+	}
+	
+	public static String mensagemChatTodos(String mensagem){
+		
+		JSONObject data = new JSONObject();
+		data.put("mensagem", mensagem);
+
+		return toJsonMessage(17,data);
+	}
+	
+	public static String mensagemChatTime(String mensagem){
+		
+		JSONObject data = new JSONObject();
+		data.put("mensagem", mensagem);
+
+		return toJsonMessage(18,data);
+	}
+	
+	
+	
+	
+	
+	private static String toJsonMessage(int code, JSONAware data){
+		JSONObject obj = new JSONObject();
+		obj.put("code",new Integer(code));
+		obj.put("data",data);
+		return obj.toJSONString();
+	}
 }
--- src/usp/wirelezzgame/server/ServerMessageEncoder.java
+++ src/usp/wirelezzgame/server/ServerMessageEncoder.java
@@ -3,82 +3,132 @@ package usp.wirelezzgame.server;
 import java.util.List;
 
 import org.json.simple.JSONArray;
+import org.json.simple.JSONAware;
 import org.json.simple.JSONObject;
 
 import usp.wirelezzgame.core.Jogador;
 import usp.wirelezzgame.core.Time;
 import usp.wirelezzgame.core.area.Area;
 import usp.wirelezzgame.core.area.AreaConquista;
+import usp.wirelezzgame.core.captcha.Captcha;
 
 @SuppressWarnings("unchecked")
 public class ServerMessageEncoder {
 
+	public static final int RECUPEROU_PONTOS = 0;
+	public static final int GASTOU_PONTOS = 1;
+
+	public static final int CHAT_TODOS = 0;
+	public static final int CHAT_TIME = 1;
+
+	public static final int AREA_DEFENDIDA = 0;
+	public static final int AREA_ATACADA = 1;
+
 	public static String nomeServer(String nomeServidor){
-		int MessageCode = 0;
 		
 		JSONObject data = new JSONObject();
 		data.put("nomeServidor", nomeServidor);
-		
-		JSONObject obj = new JSONObject();
-		obj.put("data",data);
-		obj.put("code",new Integer(MessageCode));
-		return obj.toJSONString();
+
+		return toJsonMessage(0,data);
 	}
 	
 	public static String timesData(List<Time> times){
-		int MessageCode = 2;
-
 		JSONArray timesArray = new JSONArray();
 		JSONObject time;
 		for(Time t : times){
 			time = timeEncode(t);
 			timesArray.add(time);
 		}
-		
-		JSONObject obj = new JSONObject();
-		obj.put("data",timesArray);
-		obj.put("code",new Integer(MessageCode));
-		return obj.toJSONString();
+		return toJsonMessage(2,timesArray);
 	}
 	
 	public static String areasData(List<Area> areas){
-		int MessageCode = 3;
-
 		JSONArray areasArray = new JSONArray();
 		JSONObject area;
 		for(Area a : areas){
 			area = areaEncode(a);
 			areasArray.add(area);
 		}
-		
-		JSONObject obj = new JSONObject();
-		obj.put("data",areasArray);
-		obj.put("code",new Integer(MessageCode));
-		return obj.toJSONString();
+		return toJsonMessage(3,areasArray);
 	}
 	
 	public static String jogadorIdTime(Jogador j){
-		int MessageCode = 5;
 		JSONObject jog = new JSONObject();
 		jog.put("idJogador",new Integer(j.getID()));
 		jog.put("idTime",new Integer(j.getTime()));
 		
-		
-		JSONObject obj = new JSONObject();
-		obj.put("data",jog);
-		obj.put("code",new Integer(MessageCode));
-		return obj.toJSONString();
+
+		return toJsonMessage(5,jog);
 	}
 	
 	public static String novoJogador(Jogador j){
-		int MessageCode = 6;
-		
-		JSONObject jog = jogadorEncode(j);
-		jog.put("idTime", j.getTime());		
-		
+		JSONObject jog = jogadorEncode(j); // pega outros dados deste metodo
+		jog.put("idTime", new Integer(j.getTime()));	// fica faltando o time
+		return toJsonMessage(6,jog);
+	}
+	
+	public static String mensagemCaptcha(Captcha c){
 		JSONObject obj = new JSONObject();
-		obj.put("data",jog);
-		obj.put("code",new Integer(MessageCode));
+		obj.put("idCaptcha", new Integer(c.getID()));	
+		obj.put("imagem", c.getImage());		
+		return toJsonMessage(10,obj);
+	}
+	
+	public static String mensagemResultadoCaptcha(boolean b){
+		JSONObject obj = new JSONObject();
+		obj.put("resultado",new Boolean(b));		
+		return toJsonMessage(11,obj);
+	}
+	
+	public static String mensagemPontosRecurso(Jogador j, int modo){
+		JSONObject obj = new JSONObject();
+		obj.put("pontos",new Integer(j.getPontosRecurso()));
+		obj.put("modo",new Integer(modo));		
+		return toJsonMessage(12,obj);
+	}
+	
+	public static String mensagemAlteraDefesaArea(AreaConquista a, Jogador j, int acao){
+		JSONObject obj = new JSONObject();
+		obj.put("idArea",new Integer(a.getID()));
+		obj.put("defesa",new Integer(a.getNivelDefesa()));		
+		obj.put("acao",new Integer(acao));		
+		obj.put("idJogador",new Integer(j.getID()));		
+		return toJsonMessage(13,obj);
+	}
+	
+	public static String mensagemAreaConquistada(AreaConquista a, Jogador j){
+		JSONObject obj = new JSONObject();
+		obj.put("idArea",new Integer(a.getID()));
+		obj.put("defesa",new Integer(a.getNivelDefesa()));		
+		obj.put("idTime",new Integer(a.getTimeID()));		
+		obj.put("idJogador",new Integer(j.getID()));		
+		return toJsonMessage(14,obj);
+	}
+	
+	public static String mensagemVitoriaTime(Time t){
+		JSONObject obj = new JSONObject();		
+		obj.put("idTime",new Integer(t.getID()));		
+		return toJsonMessage(15,obj);
+	}
+	
+	public static String mensagemJogadorDesconectou(Jogador j){
+		JSONObject obj = new JSONObject();		
+		obj.put("idJogador",new Integer(j.getID()));		
+		return toJsonMessage(16,obj);
+	}
+	
+	public static String mensagemChat(Jogador j, int tipo, String mensagem){
+		JSONObject obj = new JSONObject();		
+		obj.put("idJogador",new Integer(j.getID()));	
+		obj.put("tipo",new Integer(tipo));	
+		obj.put("mensagem",mensagem);		
+		return toJsonMessage(19,obj);
+	}
+	
+	private static String toJsonMessage(int code, JSONAware data){
+		JSONObject obj = new JSONObject();
+		obj.put("code",new Integer(code));
+		obj.put("data",data);
 		return obj.toJSONString();
 	}
 	
