0ccc5f31eadea217b3f3351ee531c580dbc09567
Gustavo Blanco <gu_blanco@hotmail.com>
2013-11-28 11:37:40 -0200

    Server decoder e server callback atualizado

--- src/usp/wirelezzgame/server/ServerMessageDecoder.java
+++ src/usp/wirelezzgame/server/ServerMessageDecoder.java
@@ -4,8 +4,6 @@ import org.json.simple.JSONAware;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
-import usp.wirelezzgame.client.ClientMessagesCallback;
-
 public class ServerMessageDecoder {
 	
 	private ServerMessageCallback mCallback;
@@ -24,10 +22,80 @@ public class ServerMessageDecoder {
 	
 	private void parseMessage(int code, JSONAware data){
 		switch(code){
-			case 0:
-				
+			case 1:
+				dadosJogador(data);
+			case 4:
+				timeJogador(data);
+			case 7:
+				interagirArea(data);
+			case 9:
+				responderCaptcha(data);
+			case 17:
+				mensagemChatTodos(data);
+			case 18:
+				mensagemChatTime(data);
 			break;
 		}
+	}
+	
+	//Recebe as informações do jogador e retorna um objeto jogador com esses dados
+	public void dadosJogador(JSONAware data){
+		JSONObject obj = (JSONObject)data;
+		
+		String nomeJogador = (String) obj.get("nomeJogador");		
+		String nomeCompleto = (String) obj.get("nomeCompleto");
+		String facebookID = (String) obj.get("facebookID");
+		
+		mCallback.dadosJogador(nomeJogador, nomeCompleto, facebookID);
+	}
+	
+	//Recebe informações sobre o time que o jogador irá participar
+	public void timeJogador(JSONAware data){
+		JSONObject obj = (JSONObject)data;
+		
+		Integer idTime = (Integer) obj.get("idTime");
+		
+		mCallback.timeJogador(idTime);
+	}
+	
+	//O jogador tentar interagir com uma área
+	public void interagirArea(JSONAware data){
+		JSONObject obj = (JSONObject) data;
+		
+		Integer idArea = (Integer) obj.get("idArea");
+		Integer latitude = (Integer) obj.get("latitude");
+		Integer longitude = (Integer) obj.get("longitude");
+		Integer acao = (Integer) obj.get("acao");
+		
+		mCallback.interagirArea(idArea, latitude, longitude, acao);		
+	}
+	
+	//Jogador responde um captcha
+	public void responderCaptcha(JSONAware data){
+		JSONObject obj = (JSONObject) data;
+		
+		Integer idCaptcha = (Integer) obj.get("idCaptcha");
+		String resposta = (String) obj.get("resposta");
+		
+		mCallback.responderCaptcha(idCaptcha, resposta);
+	}
+	
+	//Jogador envia uma mensagem para todos
+	public void mensagemChatTodos(JSONAware data){
+		JSONObject obj = (JSONObject) data;
+		
+		String mensagem = (String ) obj.get("mensagem");
+		
+		mCallback.mensagemChatTodos(mensagem);
+	}
+	
+	//Jogador envia uma mensagem para o time
+	public void mensagemChatTime(JSONAware data){
+		JSONObject obj = (JSONObject) data;
+		
+		String mensagem = (String) obj.get("mensagem");
+		
+		mCallback.mensagemChatTime(mensagem);
 	}
 	
 }
--- src/usp/wirelezzgame/test/ServerCallback.java
+++ src/usp/wirelezzgame/test/ServerCallback.java
@@ -3,5 +3,35 @@ package usp.wirelezzgame.test;
 import usp.wirelezzgame.server.ServerMessageCallback;
 
 public class ServerCallback implements ServerMessageCallback{
+	
+	@Override
+	public void dadosJogador(String nomeJogador, String nomeCompleto, String facebookID){
+		System.out.println("Nome jogador: " + nomeJogador + "Nome completo: " + nomeCompleto + "Facebook ID: "+ facebookID);
+	}
+	
+	@Override
+	public void timeJogador(Integer idTime){
+		System.out.println("Id time: " + idTime);
+	}
+	
+	@Override
+	public void interagirArea(Integer idArea, Integer latitude, Integer longitude, Integer acao){
+		System.out.println("Id area: " + idArea + "Latitude: " + latitude + "Longitude: " + longitude + "Acao: " + acao);
+	}
+	
+	@Override
+	public void responderCaptcha(Integer idCaptcha, String resposta){
+		System.out.println("ID captcha: " + idCaptcha + "Resposta: " + resposta);
+	}
+	
+	@Override
+	public void mensagemChatTodos(String mensagem){
+		System.out.println("Mensagem: " + mensagem);
+	}
+	
+	@Override
+	public void mensagemChatTime(String mensagem){
+		System.out.println("Mensagem: " + mensagem);
+	}
 
 }
